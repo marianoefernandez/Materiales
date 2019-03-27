265310768c238999275a3c42a1fb4dabd38d25c0
marianoefernandez <47353464+marianoefernandez@users.noreply.github.com>
2019-03-27 11:48:16 -0300

    TP1 PLANTEO

---
+++ tp1_laboratorio1 planteo/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+
+/* int flag = 0;
+
+do
+{
+    flag = 0;
+    switch ()
+    {
+    case 1:
+        op1;
+
+    case 2:
+        op2;
+
+    case 3:
+        calcular+
+        flag=1;
+
+    case 4:
+        if (flag == 1)
+        {
+
+        }
+
+        else
+        {
+    }while (op!=5)
+}
